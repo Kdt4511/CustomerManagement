80382dc13d67d839a1f60be825f60df89ea618fe
Kdt4511 <81483156+Kdt4511@users.noreply.github.com>
2022-08-14 11:48:10 +0530

    Created methods like addCustomer, getCustomer, updateCustomer, and deleteCustomer

---
+++ src/main/java/com/tutorial/basic/service/CustomerService.java
@@ -0,0 +1,60 @@
+package com.tutorial.basic.service;
+
+import com.tutorial.basic.model.Customer;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CustomerService {
+
+    private int customerIdCount = 1;
+    private List<Customer> customerList = new CopyOnWriteArrayList<>();
+
+    public Customer addCustomer(Customer customer){
+        customer.setCustomerId(customerIdCount);
+        customerList.add(customer);
+        customerList.add(customer);
+        customerIdCount++;
+        return customer;
+    }
+
+    public List<Customer> getCustomer(){
+        return customerList;
+    }
+
+    public Customer getCustomer(int customerId){
+        return customerList
+                .stream()
+                .filter(customer -> customer.getCustomerId() == customerId)
+                .findFirst()
+                .get();
+    }
+
+    public Customer updateCustomer(int customerId, Customer customer){
+             customerList
+                    .stream()
+                    .forEach(customer1 -> {
+                        if(customer1.getCustomerId() == customerId){
+                            customer1.setCustomerFirstName(customer.getCustomerFirstName());
+                            customer1.setCustomerLastName(customer.getCustomerLastName());
+                            customer1.setCustomerEmail(customer.getCustomerEmail());
+                        }
+                    });
+             return customerList
+                     .stream()
+                     .filter(customer1 -> customer1.getCustomerId() == customerId)
+                     .findFirst()
+                     .get();
+    }
+
+    public void deleteCustomer(int customerId){
+        customerList
+                .stream()
+                .forEach(customer -> {
+                    if(customer.getCustomerId() == customerId) {
+                        customerList.remove(customer);
+                    }
+                });
+    }
+
+}
