8a4d7b3947c1faf5af4577a1547c13d70b27b799
Kdt4511 <81483156+Kdt4511@users.noreply.github.com>
2022-08-27 08:25:20 +0530

    added exception class also a file containing extra commented code

--- src/main/java/com/tutorial/basic/dao/CustomerDAO.java
+++ src/main/java/com/tutorial/basic/dao/CustomerDAO.java
@@ -4,8 +4,11 @@ import com.tutorial.basic.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CustomerDAO extends CrudRepository<Customer, Integer> {
-
+    @Override
+    List<Customer> findAll();
 
 }
--- src/main/java/com/tutorial/basic/service/CustomerService.java
+++ src/main/java/com/tutorial/basic/service/CustomerService.java
@@ -1,60 +1,46 @@
 package com.tutorial.basic.service;
 
+import com.tutorial.basic.dao.CustomerDAO;
+import com.tutorial.basic.exception.CustomerNotFoundException;
 import com.tutorial.basic.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Optional;
+
 @Component
 public class CustomerService {
 
-    private int customerIdCount = 1;
-    private List<Customer> customerList = new CopyOnWriteArrayList<>();
+    @Autowired
+    private CustomerDAO customerDAO;
 
-    public Customer addCustomer(Customer customer){
-        customer.setCustomerId(customerIdCount);
-        customerList.add(customer);
-        customerIdCount++;
-        return customer;
+    public Customer addCustomer(Customer customer) {
+        return customerDAO.save(customer);
     }
 
-    public List<Customer> getCustomers(){
-        return customerList;
+    public List<Customer> getCustomers() {   // vd016
+        return customerDAO.findAll();
     }
 
-    public Customer getCustomer(int customerId){
-        return customerList
-                .stream()
-                .filter(customer -> customer.getCustomerId() == customerId)
-                .findFirst()
-                .get();
+    public Customer getCustomer(int customerId) {
+
+        Optional<Customer> optionalCustomer = customerDAO.findById(customerId);
+
+        if(!optionalCustomer.isPresent()){
+            throw new CustomerNotFoundException("Customer not available..."); //vdo20
+        }
+        return optionalCustomer.get(); //vdo 17
     }
 
-    public Customer updateCustomer(int customerId, Customer customer){
-             customerList
-                    .stream()
-                    .forEach(customer1 -> {
-                        if(customer1.getCustomerId() == customerId){
-                            customer1.setCustomerFirstName(customer.getCustomerFirstName());
-                            customer1.setCustomerLastName(customer.getCustomerLastName());
-                            customer1.setCustomerEmail(customer.getCustomerEmail());
-                        }
-                    });
-             return customerList
-                     .stream()
-                     .filter(customer1 -> customer1.getCustomerId() == customerId)
-                     .findFirst()
-                     .get();
+    public Customer updateCustomer(int customerId, Customer customer) {
+        customer.setCustomerId(customerId);
+        return customerDAO.save(customer); //vdo18
     }
 
-    public void deleteCustomer(int customerId){
-        customerList
-                .stream()
-                .forEach(customer -> {
-                    if(customer.getCustomerId() == customerId) {
-                        customerList.remove(customer);
-                    }
-                });
+    public void deleteCustomer(int customerId) {
+        customerDAO.deleteById(customerId); //vdo17
     }
 
 }
+
