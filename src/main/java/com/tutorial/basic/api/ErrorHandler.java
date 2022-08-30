ae9707d99854bfce5e748abf0797966a31520d08
Kdt4511 <81483156+Kdt4511@users.noreply.github.com>
2022-08-30 09:34:47 +0530

    Added error handling class with a customized message

---
+++ src/main/java/com/tutorial/basic/api/ErrorHandler.java
@@ -0,0 +1,28 @@
+package com.tutorial.basic.api;
+
+
+import com.tutorial.basic.exception.ApplicationError;
+import com.tutorial.basic.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ApplicationError> handleCustomerNotFoundException
+            (CustomerNotFoundException exception, WebRequest webRequest) {
+
+        ApplicationError error = new ApplicationError();
+        error.setCode(10);
+        error.setMessage(exception.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/com/tutorial/basic/exception/CustomerNotFoundException.java
+++ src/main/java/com/tutorial/basic/exception/CustomerNotFoundException.java
@@ -1,9 +1,5 @@
 package com.tutorial.basic.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND) //vd020
+ //vd020
 public class CustomerNotFoundException extends RuntimeException {
 
     public CustomerNotFoundException(String message){
