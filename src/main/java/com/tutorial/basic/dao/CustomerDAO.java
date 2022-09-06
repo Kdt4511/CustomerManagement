89f3c624dd3fd6e8310c97451d10c3271c139fbe
Kdt4511 <81483156+Kdt4511@users.noreply.github.com>
2022-09-06 18:40:09 +0530

    override the remaining methods

--- src/main/java/com/tutorial/basic/dao/CustomerDAO.java
+++ src/main/java/com/tutorial/basic/dao/CustomerDAO.java
@@ -5,10 +5,43 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CustomerDAO extends CrudRepository<Customer, Integer> {
     @Override
     List<Customer> findAll();
 
+    @Override
+    <S extends Customer> S save(S entity);
+
+    @Override
+    <S extends Customer> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    Optional<Customer> findById(Integer integer);
+
+    @Override
+    boolean existsById(Integer integer);
+
+    @Override
+    Iterable<Customer> findAllById(Iterable<Integer> integers);
+
+    @Override
+    long count();
+
+    @Override
+    void deleteById(Integer integer);
+
+    @Override
+    void delete(Customer entity);
+
+    @Override
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    void deleteAll(Iterable<? extends Customer> entities);
+
+    @Override
+    void deleteAll();
 }
